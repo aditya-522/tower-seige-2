935a7ca7cc6ff438f81370f266ce9f80f0f30a30
aditya-522 <71328647+aditya-522@users.noreply.github.com>
2020-11-23 18:47:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,83 @@
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint 
+
+var engine, world;
+var ground; 
+
+var ground, stand1, stand2;
+
+function preload(){
+  polygonImage = loadImage("polygon.png");
+}
+
+function setup() {
+  createCanvas(1000,700);
+ 
+  engine = Engine.create();
+  world = engine.world;
+ 
+  ground=new Ground(width/2,height,width,20) 
+
+  stand1 = new Ground(500,500,350,15);
+ 
+  block2=new Box(400,470,50,50)
+  block3=new Box(450,470,50,50)
+  block4=new Box(500,470,50,50)
+  block5=new Box(550,470,50,50)
+  block6=new Box(600,470,50,50)
+ 
+  block9=new Box(450,420,50,50)
+  block10=new Box(500,420,50,50)
+  block11=new Box(550,420,50,50)
+
+  block12=new Box(500,370,50,50)
+
+  polygon = Bodies.circle(50,200,40);
+  World.add(world,polygon);
+
+  slingshot = new SlingShot(polygon,{x:250,y:300})
+  
+}
+function draw() {
+  background(220);  
+  Engine.update(engine);
+  ground.display()
+
+  stand1.display()
+  
+  fill("red")
+  block2.display()
+  block3.display()
+  block4.display()
+  block5.display()
+  block6.display()
+  fill("blue")
+  block9.display()
+  block10.display()
+  block11.display()
+  fill("yellow")
+  block12.display()
+  slingshot.display()
+
+  imageMode(CENTER)
+  image(polygonImage,polygon.position.x, polygon.position.y, 60,60)
+
+}
+
+function keyPressed(){
+	if(keyCode === 32){
+    Matter.Body.setPosition(polygon, {x:250,y:300})
+		slingshot.attach(polygon.body)
+	}
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(polygon, {x:mouseX,y:mouseY})
+}
+
+function mouseReleased(){
+    slingshot.fly();
+}
